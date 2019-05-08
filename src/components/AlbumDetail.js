f0248247f2c7282b5ac397035d82bd7e10ec88c0
naridev <narishin.ns@gmail.com>
2019-05-07 23:35:30 -0700

    card and cardsection components

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import Card from './Card';
+import CardSection from './CardSection';
 
 const AlbumDetail = (props) => {
     return (
-        <View>
-            <Text>{props.album.title}</Text>
-        </View>
+        <Card>
+            <CardSection>
+                <View>
+                    <Text>{props.album.thumbnail_image}</Text>
+                </View>
+                <View>
+                    <Text>{props.album.title}</Text>
+                    <Text>{props.album.artist}</Text>
+                </View>
+            </CardSection>
+        </Card>
     );
 };
 
