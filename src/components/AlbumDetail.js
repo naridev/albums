b6194dc599e00777be6b5cbdb5be907fcaff27bd
naridev <narishin.ns@gmail.com>
2019-05-08 00:25:47 -0700

    Button style and linking

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { View, Text, Image } from 'react-native';
+import { View, Text, Image, Linking } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
+import Button from './Button';
 
 const AlbumDetail = ( {album} ) => {
-    const {title, artist, thumbnail_image, image} = album;
+    const {title, artist, thumbnail_image, image, url} = album;
     return (
         <Card>
             <CardSection>
@@ -17,9 +18,14 @@ const AlbumDetail = ( {album} ) => {
                     <Text>{artist}</Text>
                 </View>
             </CardSection>
+
             <CardSection>
                 <Image style = {styles.imageStyle}
                 source = {{ uri: image }} />
+            </CardSection>
+
+            <CardSection>
+                <Button onPress = {() => Linking.openURL(url)} />
             </CardSection>
         </Card>
     );
